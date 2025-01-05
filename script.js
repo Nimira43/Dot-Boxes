d97aa6c3fe0ad213e7ddbfc5932a22838724f939
Nimira43 <n.rabone72@aol.com>
2025-01-05 21:03:42 +0000

    Created new files and brought in new reviewed code.

---
+++ script.js
@@ -0,0 +1,29 @@
+import { height, gridSize, fps, delayEnd, width, cell, stroke, dot, margin, colourBoard, colourBorder, colourDot, colourAi, colourAiLight, colourPlayer, colourPlayerLight, tie } from './variables'
+
+
+
+
+
+
+// Old code
+
+// let canvasEl = document.createElement('canvas')
+// canvasEl.height = height
+// canvasEl.width = width
+// document.body.appendChild(canvasEl)
+// let canvasRect = canvasEl.getBoundingClientRect()
+
+// const ctx = canvasEl.getContext('2d')
+// ctx.lineWidth = stroke
+// ctx.textAlign = 'center'
+// ctx.textBaseline = 'middle'
+
+// function playGame() {
+//   requestAnimationFrame(playGame)
+//   drawBoard()
+// }
+
+// function drawBoard() {
+//   ctx.fillStyle = colourBoard
+//   ctx.strokeStyle
+// }
