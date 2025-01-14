2942452a23fe653f774f07a355caf0de75c7c136
Nimira43 <n.rabone72@aol.com>
2025-01-14 01:58:35 +0000

    Starting again with the refactor.

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-import { height, gridSize, fps, delayEnd, width, cell, stroke, dot, margin, colourBoard, colourBorder, colourDot, colourAi, colourAiLight, colourPlayer, colourPlayerLight, colourTie, sideObject, textSizeCell, textPlayer, textAi, textSizeTop, textTie, textWin } from './variables.js'
+import { height, gridSize, fps, delayEnd, width, cell, stroke, dot, margin, colourBoard, colourBorder, colourDot, colourAi, colourAiLight, colourPlayer, colourPlayerLight, colourTie, sideObject, textSizeCell, textPlayerSm, textPlayer, textAiSm, textAi, textSizeTop, textTie, textWin } from './variables.js'
 
 import { getColour, getText, getGridX, getGridY } from './getters.js'
 
-import { drawGrid, highlightGrid } from './grid.js'
+let canvasEl = document.createElement('canvas')
+canvasEl.height = height
+canvasEl.width = width
+document.body.appendChild(canvasEl)
+let canvasRect = canvasEl.getBoundingClientRect()
 
-
-
-
+const ctx = canvasEl.getContext('2d')
+ctx.lineWidth = stroke
+ctx.textAlign = 'center'
+ctx.textBaseline = 'middle'
 
 let currentCells
 let playersTurn 
 let squares
 let scoreAI 
 let scoreRI 
-export let timeEnd
+let timeEnd
 
 canvasEl.addEventListener('mousemove', highlightGrid)
   
@@ -34,19 +39,139 @@ function click(e) {
   selectSide()
 }
 
+function drawBoard() {
+  ctx.fillStyle = colourBoard
+  ctx.strokeStyle = colourBorder
+  ctx.fillRect(0, 0, width, height)
+  ctx.strokeRect(
+    stroke / 4,
+    stroke / 4,
+    width - stroke / 2,
+    height - stroke / 2
+  )
+}
 
-
-
+function drawDot(x, y) {
+  ctx.fillStyle = colourDot
+  ctx.beginPath()
+  ctx.arc(x, y, dot, 0, Math.PI * 2)
+  ctx.fill()
+}
  
+function drawGrid() {
+  for (let i = 0; i < gridSize + 1; i++) {
+    for (let j = 0; j < gridSize + 1; j++) {
+      drawDot(getGridX(j), getGridY(i))
+    }
+  }
+}
+
+function drawLine(x0, y0, x1, y1, colour) {
+  ctx.strokeStyle = colour
+  ctx.beginPath()
+  ctx.moveTo(x0, y0)
+  ctx.lineTo(x1, y1)
+  ctx.stroke()
+}
+
+function drawScores() {
+  let colourAI = playersTurn ? colourAiLight : colourAi
+  let colourRI = playersTurn ? colourPlayer : colourPlayerLight
+
+  drawText(
+    textPlayer, 
+    width * 0.25, 
+    margin * 0.25,
+    colourRI,
+    textSizeTop
+  )
+
+  drawText(
+    scoreRI,
+    width * 0.25,
+    margin * 0.6,
+    colourRI,
+    textSizeTop * 2
+  )
+
+  drawText(
+    textAi, 
+    width * 0.75, 
+    margin * 0.25,
+    colourAI,
+    textSizeTop 
+  )
+
+  drawText(
+    scoreAI,
+    width * 0.75,
+    margin * 0.6,
+    colourAI,
+    textSizeTop * 2
+  )
+
+  if (timeEnd > 0) {
+    timeEnd--
+    if (scoreRI == scoreAI) {
+      drawText(
+        textTie,
+        width * 0.5,
+        margin * 0.6,
+        colourTie,
+        textSizeTop
+      ) 
+    } else {
+      let playerWins = scoreRI > scoreAI
+      let colour = playerWins ? colourPlayer : colourAi
+      let text = playerWins ? textPlayer : textAi
+      drawText(
+        text,
+        width * 0.5,
+        margin * 0.5,
+        colour,
+        textSizeTop
+      )
+      drawText(
+        textWin, 
+        width * 0.5,
+        margin * 0.7,
+        colour,
+        textSizeTop
+      )
+    }
+    if (timeEnd == 0) {
+      newGame()
+    }
+  }
+}
+
+function drawSquares() {
+  for (let row of squares) {
+    for (let square of row) {
+      square.drawSides()
+      square.drawFill()
+    }
+  }
+}
+
+function drawText(text, x, y, colour, size) {
+  ctx.fillStyle = colour
+  ctx.font = `${size}px sans-serif`
+  ctx.fillText(text, x, y)
+}
 
 
 
+function highlightGrid(e) {
+  if (timeEnd > 0) {
+    return
+  }
+  let x = e.clientX - canvasRect.left
+  let y = e.clientY - canvasRect.top
+  highlightSide(x, y)
+}
 
-
-
-
-
-export function highlightSide(x, y) {
+function highlightSide(x, y) {
   for (let row of squares) {
     for (let square of row) {
       square.highlight = null
