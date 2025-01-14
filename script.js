b28d88d053ad01e647616d6849551c144b280471
Nimira43 <n.rabone72@aol.com>
2025-01-14 01:21:01 +0000

    Moved drawing functions to drawingFunctions.js

--- script.js
+++ script.js
@@ -34,118 +34,17 @@ function click(e) {
   selectSide()
 }
 
-function drawBoard() {
-  ctx.fillStyle = colourBoard
-  ctx.strokeStyle = colourBorder
-  ctx.fillRect(0, 0, width, height)
-  ctx.strokeRect(
-    stroke / 4,
-    stroke / 4,
-    width - stroke / 2,
-    height - stroke / 2
-  )
-}
 
-export function drawDot(x, y) {
-  ctx.fillStyle = colourDot
-  ctx.beginPath()
-  ctx.arc(x, y, dot, 0, Math.PI * 2)
-  ctx.fill()
-}
+
+
  
-function drawLine(x0, y0, x1, y1, colour) {
-  ctx.strokeStyle = colour
-  ctx.beginPath()
-  ctx.moveTo(x0, y0)
-  ctx.lineTo(x1, y1)
-  ctx.stroke()
-}
 
-function drawScores() {
-  let colourAI = playersTurn ? colourAiLight : colourAi
-  let colourRI = playersTurn ? colourPlayer : colourPlayerLight
 
-  drawText(
-    textPlayer, 
-    width * 0.25, 
-    margin * 0.25,
-    colourRI,
-    textSizeTop
-  )
 
-  drawText(
-    scoreRI,
-    width * 0.25,
-    margin * 0.6,
-    colourRI,
-    textSizeTop * 2
-  )
 
-  drawText(
-    textAi, 
-    width * 0.75, 
-    margin * 0.25,
-    colourAI,
-    textSizeTop 
-  )
 
-  drawText(
-    scoreAI,
-    width * 0.75,
-    margin * 0.6,
-    colourAI,
-    textSizeTop * 2
-  )
 
-  if (timeEnd > 0) {
-    timeEnd--
-    if (scoreRI == scoreAI) {
-      drawText(
-        textTie,
-        width * 0.5,
-        margin * 0.6,
-        colourTie,
-        textSizeTop
-      ) 
-    } else {
-      let playerWins = scoreRI > scoreAI
-      let colour = playerWins ? colourPlayer : colourAi
-      let text = playerWins ? textPlayer : textAi
-      drawText(
-        text,
-        width * 0.5,
-        margin * 0.5,
-        colour,
-        textSizeTop
-      )
-      drawText(
-        textWin, 
-        width * 0.5,
-        margin * 0.7,
-        colour,
-        textSizeTop
-      )
-    }
-    if (timeEnd == 0) {
-      newGame()
-    }
-  }
-}
 
-function drawSquares() {
-  for (let row of squares) {
-    for (let square of row) {
-      square.drawSides()
-      square.drawFill()
-    }
-  }
-}
-
-function drawText(text, x, y, colour, size) {
-  ctx.fillStyle = colour
-  ctx.font = `${size}px sans-serif`
-  ctx.fillText(text, x, y)
-}
 
 export function highlightSide(x, y) {
   for (let row of squares) {
